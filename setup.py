ebc8f6fc440d92d4cb70da3e80afff2247c6fce0
Gabriele Orlando <orlando.gabriele89@gmail.com>
2025-05-09 13:19:47 +0200

    pip setup.py. tested local install without dependances

---
+++ setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+setup(
+
+     name='croz',
+     zip_safe=False,
+     include_package_data=False,
+
+     version='0.0.1',
+
+     author="Gabriele Orlando",
+
+     author_email="gabriele.orlando@umontpellier.be",
+
+     description="A method based on pyuul to score a protein (or other biological structure) model into a cryoEM electrondensity",
+
+     long_description=long_description,
+
+     long_description_content_type="text/markdown",
+
+     url="https://github.com/grogdrinker/croz",
+     
+     packages=['croz',"croz.src"],
+     package_dir={'croz': 'croz/',"croz.src":"croz/src"},
+
+     install_requires=["torch","numpy", "mrcfile","pyuul","matplotlib",'madrax @ git+https://bitbucket.org/grogdrinker/madrax/'],
+
+     classifiers=[
+
+         "Programming Language :: Python :: 3",
+
+         "Operating System :: OS Independent",
+
+     ],
+
+ )
